a77598c03e3917133d8be14775fd296935a5a26c
Kristi Kacaj <kristik.web@gmail.com>
2020-09-13 16:08:54 -0400

    completed form

--- client/src/components/Form/index.js
+++ client/src/components/Form/index.js
@@ -1,14 +1,28 @@
-const router = require("express").Router();
-const booksController = require("../../controllers/booksControllers");
+import React from "react";
 
-router.route("/")
-  .get(booksController.findAll)
-  .post(booksController.create);
 
-router
-  .route("/:id")
-  .get(booksController.findById)
-  .put(booksController.update)
-  .delete(booksController.remove);
+function Form(props){
+return (
+  <div className="container">
+  <form>
+    <div className="form-group">
+      <label htmlFor="search"><h2>Search for and save Books of Interest</h2></label>
+      <input
+        onChange={props.handleInputChange}
+        value={props.search}
+        name="search"
+        type="text"
+        className="form-control"
+        placeholder="Search a Book"
+        id="search"
+      />
+      <button onClick={props.handleFormSubmit} className="btn btn-dark mt-3 mb-5">
+        Search
+        </button>
+    </div>
+  </form>
+  </div>
+);
+}
 
-module.exports = router;
+export default Form;
